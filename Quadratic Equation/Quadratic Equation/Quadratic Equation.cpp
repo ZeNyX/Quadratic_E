7585aa2953538a12ea11db9fc97cd0fcd540595b
ZeNyX <vlad.sokolovsky666@gmail.com>
2017-08-31 00:14:41 +0300

    Hell_upadte0.1

--- Quadratic Equation/Quadratic Equation/Quadratic Equation.cpp
+++ Quadratic Equation/Quadratic Equation/Quadratic Equation.cpp
@@ -10,18 +10,30 @@ int main()
 	int a, b, c, g;
 	int D, x, y;
 	cout << "Welcome to the Quadratic Equation!" << endl;
+	cout << "Coded by ShadoW";
 	cout << "Please enter A type - " << endl;
 	cin >> a;
-		cout << a << endl;
-		cout << "Please enter B type - " << endl;
-		cin >> b;
-		cout << "Please enter C type - " << endl;
-		cin >> c;
-		cout << "A - " << a << endl;
-		cout << "B - " << b << endl;
-		cout << "C - " << c << endl;
-		D = b^2-4*a*c;
-		cout << D;
+	cout << a << endl;
+	cout << "Please enter B type - " << endl;
+	cin >> b;
+	cout << "Please enter C type - " << endl;
+	cin >> c;
+	cout << "A - " << a << endl;
+	cout << "B - " << b << endl;
+	cout << "C - " << c << endl;
+	D = b ^ 2 - 4 * a*c;
+	if (D > 0) {
+		cout << "Your Discriminant is - " << D;
+	}
+	if (D = 0) {
+		cout << "D - " << D << endl;
+		cout << "You equation <= 0!";
+	}
+	else {
+		cout << "D - " << D;
+	cout << "No roots!";
+}
+		int x1, x2;
 		cin >> g;
     return 0;
 }
