685497232f1dbc319391fa6ebab83c28ec7eae98
ZeNyX <vlad.sokolovsky666@gmail.com>
2017-08-30 22:19:01 +0300

    first

---
+++ Quadratic Equation/Quadratic Equation/Quadratic Equation.cpp
@@ -0,0 +1,25 @@
+// Quadratic Equation.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int a, b, c, g;
+	cout << "Welcome to the Quadratic Equation!" << endl;
+	cout << "Please enter A type - " << endl;
+	cin >> a;
+		cout << a << endl;
+		cout << "Please enter B type - " << endl;
+		cin >> b;
+		cout << "Please enter C type - " << endl;
+		cin >> c;
+		cout << "A - " << a << endl;
+		cout << "B - " << b << endl;
+		cout << "C - " << c << endl;
+		cin >> g;
+    return 0;
+}
+
