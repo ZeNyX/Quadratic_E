472c2f1c47aaaa5a72f64c3cd8e1d61a30731428
ZeNyX <vlad.sokolovsky666@gmail.com>
2017-08-31 02:02:50 +0300

    hell_updt_alpha-2

--- Quadratic Equation/Quadratic Equation/Quadratic Equation.cpp
+++ Quadratic Equation/Quadratic Equation/Quadratic Equation.cpp
@@ -11,6 +11,7 @@ int main()
 	int D, x, y; // nxt lib
 	cout << "Welcome to the Quadratic Equation!" << endl;
 	cout << "Coded by ZeNyX" << endl;
+	cout << "Me in GitHub - https://github.com/ZeNyX" << endl;
 	cout << "Please enter A type - " << endl;
 	cin >> a;
 	cout << a << endl;
@@ -19,27 +20,35 @@ int main()
 	cout << "Please enter C type - " << endl;
 	cin >> c;
 	cout << "A - " << a << endl;
-	cout << "B - " << b << endl;
+	cout << "B - " << b << endl; 
 	cout << "C - " << c << endl;
 	D = b ^ 2 - 4 * a*c; // func
 	if (D > 0) {
 		cout << "Your Discriminant is - " << D << endl;
+		
+		int x1, xx;
+		x1 = (-b + sqrt(D)) / 2 * a;
+		xx = (-b - sqrt(D)) / 2 * a;
+		cout << "x1 = " << x1 << endl;
+		cout << "x2 = " << xx << endl;
 	}
-	if (D = 0) {
+	if (D == 0) {
 		cout << "Your Discriminant is - " << D << endl;
 		cout << "You equation have one root!" << endl;
+		
+		int x1, xx;
+		x1 = (-b + sqrt(D)) / 2 * a;
+		xx = (-b - sqrt(D)) / 2 * a;
+		cout << "x1 = " << x1 << endl;
+		cout << "x2 = " << xx << endl;
 	}
 	if (D < 0) {
 		cout << "Your Discriminant is - " << D;
 	cout << "No roots!";
 }
-		int x1, x2;
-		x1 = (-b + sqrt(D)) / 2 * a;
-		cout << "x1 = " << x1 << endl;
-		x2 = (-b - sqrt(D)) / 2 * a;
-		cout << "x2 = " << x2 << endl;
 		cout << "Please push any key & press ENTER to close...";
 		cin >> none;
+
     return 0;
 }
 
